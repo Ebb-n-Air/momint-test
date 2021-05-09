d0a8124ff08132ebe4eea9aa4bf52460bcc045e5
Paul Harrington <paul.dennis.harrington@gmail.com>
2021-05-09 19:53:59 +0200

    using regex to match file data

--- api/src/controllers/file.controller.ts
+++ api/src/controllers/file.controller.ts
@@ -7,13 +7,15 @@ export const uploadFile = async (req: Request, res: Response) => {
     try {
         console.log('upload hit');
         let uf = req.body.media_data;
-        let ext = req.body.extension;
         let fn = req.body.file_name;
-        let file = btoa(uf.split(':base64,').pop());
+        const regex = /^data:.+\/(.+);base64,(.*)$/;
+        let matches = uf.match(regex);
+        let ext = matches[1];
+        let data = matches[2]; 
         const basePath ="C:/S/R/momint/momint-test/api/src/media-uploads/"; 
         const src = basePath + fn; // TODO: replace hardcoded src val with environment variable
         console.log(src);
-        fs.writeFile(src, file, {encoding: 'base64'}, function (err) {
+        fs.writeFile(src, data, {encoding: 'base64'}, function (err) {
             const srcStream = fs.createReadStream(src);
             const dest = src + ".gz";
             const destStream = fs.createWriteStream(dest);
