cc4ded34461bc77d8b731cdc96fe426e35ade631
Paul Harrington <paul.dennis.harrington@gmail.com>
2021-05-09 18:12:03 +0200

    connections to backend

--- front-end/src/app/home/home.page.ts
+++ front-end/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  constructor(private http: HttpClient) {}
+  private media_file:any;
+  private downloadedFile:any;
+  public onFileSelected(event) {
+    event.preventDefault();
+    const file:File = event.target.files[0];
+    if (file)
+    {
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      
+      reader.onload = (event:any) => {
+        console.log('file selected');
+        this.http.post("http://localhost:3000/api/files/uploadFile", {
+          media_data: event.target.result,
+          file_name: file.name,
+          extension: file.type
+        })
+        .subscribe(data => {
+          console.log(data);    
+        });
+      }
+    }
+  }
 }
